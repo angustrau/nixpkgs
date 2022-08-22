d0b013dfc1ba1a80fe786ce8467b575d031b244c
Emily Trau <me@angus.ws>
2022-08-22 17:09:11 +1000

    musl: init

---
+++ pkgs/stdenv/linux/bootstrap0/musl/default.nix
@@ -0,0 +1,65 @@
+{ fetchurl, runKaem, tcc, gnumake, sed, coreutils }:
+let
+  version = "1.2.3";
+  src = builtins.fetchTarball {
+    url = "https://musl.libc.org/releases/musl-${version}.tar.gz";
+    sha256 = "00r0pcawgid6bfm8l7vhls3zs1ksca3vs58vjjjq9asarnwyfg31";
+  };
+
+  syscall_h = runKaem {
+    name = "syscall.h-${version}";
+    buildInputs = [ sed coreutils ];
+    scriptText = ''
+      cp --preserve="mode" ${src}/arch/i386/bits/syscall.h.in ./syscall.h
+      sed -i -n -e s/__NR_/SYS_/p ./syscall.h
+      catm ''${out} ${src}/arch/i386/bits/syscall.h.in syscall.h
+    '';
+  };
+in
+runKaem {
+  name = "musl-${version}";
+  buildInputs = [ tcc gnumake sed coreutils ];
+  scriptText = ''
+    cp -r ${src} src
+    chmod -R a+rw ./src
+    cd src/
+
+    catm config.mak
+
+    replace --file src/signal/i386/sigsetjmp.s --output src/signal/i386/sigsetjmp.s --match-on "jecxz 1f" --replace-with "cmp %ecx,0\nje 1f"
+    replace --file src/include/features.h --output src/include/features.h --match-on "__weak__, " --replace-with ""
+    rm -rf src/complex src/math/i386/ src/math/sqrtl.c
+
+    mkdir -p include/bits/ lib/
+    cp --preserve="mode" ${syscall_h} include/bits/syscall.h
+
+    make \
+      prefix=''${out} \
+      CC=tcc \
+      ARCH=i386 \
+      CROSS_COMPILE= \
+      AR="tcc -ar" \
+      RANLIB=true \
+      CFLAGS="-DSYSCALL_NO_TLS" \
+      SHARED_LIBS= \
+      TOOL_LIBS= \
+      EMPTY_LIBS= \
+      ALL_TOOLS=
+
+    chmod -R a+r .
+    make \
+      prefix=''${out} \
+      CC=tcc \
+      ARCH=i386 \
+      CROSS_COMPILE= \
+      AR="tcc -ar" \
+      RANLIB=true \
+      CFLAGS="-DSYSCALL_NO_TLS" \
+      SHARED_LIBS= \
+      TOOL_LIBS= \
+      EMPTY_LIBS= \
+      ALL_TOOLS= \
+      install
+  '';
+  # Disable TOOL_LIBS, fails to build without /bin/sh
+}

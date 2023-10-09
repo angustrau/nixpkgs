673978b745aabbce8c5b6184b7affb4572e20a8f
Emily Trau <emily@downunderctf.com>
2023-10-08 22:55:29 -0700

    minimal-bootstrap.binutils-static: init at 2.41

--- pkgs/os-specific/linux/minimal-bootstrap/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/default.nix
@@ -30,6 +30,12 @@ lib.makeScope
       gnutar = gnutar-musl;
     };
 
+    binutils-static = callPackage ./binutils/static.nix {
+      gcc = gcc-latest;
+      gnumake = gnumake-musl;
+      gnutar = gnutar-latest;
+    };
+
     busybox-static = callPackage ./busybox/static.nix {
       gcc = gcc-latest;
       gnumake = gnumake-musl;
@@ -224,6 +230,7 @@ lib.makeScope
       echo ${bash.tests.get-version}
       echo ${bash_2_05.tests.get-version}
       echo ${binutils.tests.get-version}
+      echo ${binutils-static.tests.get-version}
       echo ${busybox-static.tests.get-version}
       echo ${bzip2.tests.get-version}
       echo ${coreutils-musl.tests.get-version}

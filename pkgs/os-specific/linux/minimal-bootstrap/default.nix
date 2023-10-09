1bd0d97d08b736005d535a31f91aeb3239916f29
Emily Trau <emily@downunderctf.com>
2023-10-08 21:58:32 -0700

    minimal-bootstrap.coreutils-static: init at 9.4

--- pkgs/os-specific/linux/minimal-bootstrap/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/default.nix
@@ -50,6 +50,11 @@ lib.makeScope
       gnumake = gnumake-musl;
       gnutar = gnutar-musl;
     };
+    coreutils-static = callPackage ./coreutils/static.nix {
+      gcc = gcc-latest;
+      gnumake = gnumake-musl;
+      gnutar = gnutar-latest;
+    };
 
     diffutils = callPackage ./diffutils {
       bash = bash_2_05;
@@ -216,6 +221,7 @@ lib.makeScope
       echo ${busybox-static.tests.get-version}
       echo ${bzip2.tests.get-version}
       echo ${coreutils-musl.tests.get-version}
+      echo ${coreutils-static.tests.get-version}
       echo ${diffutils.tests.get-version}
       echo ${findutils.tests.get-version}
       echo ${gawk-mes.tests.get-version}
